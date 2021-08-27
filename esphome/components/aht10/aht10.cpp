3ab1538e63d4897daecf831100ca25fb9b10d68f
mahlernim <33252024+mahlernim@users.noreply.github.com>
2021-08-28 01:50:10 +0900

    Update aht10.cpp
    AHT20 not updating but reporting the same value over and over. May be due to insufficient wait time.

--- esphome/components/aht10/aht10.cpp
+++ esphome/components/aht10/aht10.cpp
@@ -21,8 +21,8 @@ namespace aht10 {
 static const char *const TAG = "aht10";
 static const uint8_t AHT10_CALIBRATE_CMD[] = {0xE1};
 static const uint8_t AHT10_MEASURE_CMD[] = {0xAC, 0x33, 0x00};
-static const uint8_t AHT10_DEFAULT_DELAY = 5;    // ms, for calibration and temperature measurement
-static const uint8_t AHT10_HUMIDITY_DELAY = 30;  // ms
+static const uint8_t AHT10_DEFAULT_DELAY = 10;    // ms, for calibration and temperature measurement
+static const uint8_t AHT10_HUMIDITY_DELAY = 50;  // ms
 static const uint8_t AHT10_ATTEMPTS = 3;         // safety margin, normally 3 attempts are enough: 3*30=90ms
 
 void AHT10Component::setup() {
